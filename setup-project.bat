4933253cd1250f8a4e09eba97f95823960aa1d09
Dender46 <dmitriy.bagandov@gmail.com>
2024-05-13 19:00:19 +0300

    Add downloading and setuping of box2d lib

--- setup-project.bat
+++ setup-project.bat
@@ -7,15 +7,40 @@ if not exist vendor\ (
 )
 
 if not exist vendor\raylib\ (
+    echo ===== Raylib: downloading raylib binaries and headers =====
     powershell Invoke-WebRequest https://github.com/raysan5/raylib/releases/download/5.0/raylib-5.0_win64_mingw-w64.zip -OutFile vendor/raylib.zip
-    echo Raylib: downloaded
+
+    echo ===== Raylib: extracting =====
     powershell Expand-Archive vendor/raylib.zip -DestinationPath vendor
-    echo Raylib: extracted .zip file
+
+    echo ===== Raylib: renaming and cleaning up =====
     ren vendor\raylib-5.0_win64_mingw-w64 raylib
     del vendor\raylib.zip
-    echo Raylib: renamed and cleaned up
 ) else (
     echo Raylib already exists in .\vendor\
+)
+
+if not exist vendor\box2d\ (
+    echo ===== Box2d: cloning repo =====
+    git clone https://github.com/erincatto/box2d.git vendor\box2d
+
+    pushd vendor\box2d
+        echo ===== Box2d: cmake building =====
+        mkdir build && cd build
+        cmake ..
+        cmake --build . --config Release
+
+        echo =====Box2d: moving lib =====
+        cd ..
+        mkdir lib
+        move build\bin\Release\box2d.lib lib\
+
+        echo =====Box2d: cleaning up cmake build =====
+        del   /s /q build
+        rmdir /s /q build
+    popd
+) else (
+    echo Box2d already exists in .\vendor\
 )
 
 echo Script completed
