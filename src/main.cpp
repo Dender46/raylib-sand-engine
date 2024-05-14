a70b2b026f209d0ecef52334944ea35f12c568f7
Dender46 <dmitriy.bagandov@gmail.com>
2024-05-14 19:45:37 +0300

    Add axis labels

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,66 @@
-#include "raylib.h"
+#include <iostream>
+#include <cmath>
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+#include "raylib.h"
+#include "raymath.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+struct TextWithPivot
+{
+public:
+    TextWithPivot(Font _font, const char* _label, Vector2 _pivot, int _fontSize)
+        : label(_label), pivot(_pivot), fontSize(_fontSize), size(MeasureTextEx(_font, _label, _fontSize, 2))
+    {}
+
+    void Draw(int posX, int posY, Color color)
+    {
+        DrawText(label, posX - (size.x * pivot.x), posY - (size.y * pivot.y), fontSize, color);
+    }
+
+    void Draw(int posX, int posY, float rotation, Color color)
+    {
+        Vector2 pos{ (float)posX, (float)posY };
+        Vector2 pivotSizeBased{ 
+            size.x * pivot.x,
+            size.y * pivot.y,
+        };
+        DrawTextPro(defaultFont, label, pos, pivotSizeBased, rotation, fontSize, 1.0f, color);
+    }
+
+    const Font defaultFont{ GetFontDefault() };
+    const char* label;
+    const int fontSize;
+    Vector2 pivot;
+    const Vector2 size;
+};
+
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth{ 800 };
+    const int screenHeight{ 450 };
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(60);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    auto defaultFont{ GetFontDefault() };
+    TextWithPivot XAxisLabel(defaultFont, "X Axis", {0.5f, 1.0f}, 28);
+    TextWithPivot YAxisLabel(defaultFont, "Y Axis", {0.5f, 0.0f}, 28);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    float rotation{};
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            XAxisLabel.Draw(screenWidth / 2, screenHeight, GRAY);
+            YAxisLabel.Draw(0, screenHeight / 2, -90.0f, GRAY);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
 }
