541525bdd7e9c2602dd86e9b9639314027d106e2
Dender46 <dmitriy.bagandov@gmail.com>
2024-08-30 16:30:06 +0300

    Add vars for profiler table options

--- src/profiler/profilerRLDisplay.hpp
+++ src/profiler/profilerRLDisplay.hpp
@@ -44,15 +44,23 @@ inline void DrawProfilerResults(int screenWidth, int screenHeight, u64 _totalCyc
     TableColumn tableColumns[] = {
         {"anchorName", 0},
         {"elapsedTSC", 0},
-        {"elapsedTimeInS", 0},
+        {"elapsedSecs.", 0},
         {"%"},
-        {"% w/ children", 0},
+        {"% w/ childs", 0},
         {"processedMB", 0},
         {"GB/S", 0},
     };
+
+    // TABLE STYLE OPTIONS
+    u32 tableLeftPadding{ 10 };
+    u32 tableTopPadding{ 10 };
+
+    u32 elementWidthPadding{ 20 };
+    f32 heightMultiplier{ 1.5f };
+
     Font font{ GetFontDefault() };
+    u32 fontSizeHeader{ 20 };
     u32 fontSize{ 28 };
-    u32 widthPadding{ 20 };
 
     constexpr u8 tableCountCol{ sizeof(tableColumns) / sizeof(tableColumns[0]) };
     u64 tableCountRow{ profilerReports.size() + 1 };
@@ -74,44 +82,44 @@ inline void DrawProfilerResults(int screenWidth, int screenHeight, u64 _totalCyc
         ClearBackground(RAYWHITE);
         BeginDrawing();
 
-        u32 widthSum{ 0 };
+        u32 widthSum{ tableLeftPadding };
         char strBuffer[64];
         for (u32 i = 0; i < tableCountCol; i++)
         {
-            DrawText(tableColumns[i].mName, widthSum, 0, fontSize, BLACK);
-            widthSum += tableColumns[i].mWidth + widthPadding;
+            DrawText(tableColumns[i].mName, widthSum, 0, fontSizeHeader, BLACK);
+            widthSum += tableColumns[i].mWidth + elementWidthPadding;
         }
         for (u32 i = 0; i < tableCountRow-1; i++)
         {
-            widthSum = 0;
-            u32 height{ (i+1) * (fontSize*1.5) };
+            widthSum = tableLeftPadding;
+            u32 height{ (i+1) * (fontSize*heightMultiplier) };
             i32 colIndex{ -1 };
             DrawText(profilerReports[i].mAnchor.label, widthSum, height, fontSize, BLACK);
-            widthSum += tableColumns[++colIndex].mWidth + widthPadding;
+            widthSum += tableColumns[++colIndex].mWidth + elementWidthPadding;
 
             std::sprintf(strBuffer, "%llu", profilerReports[i].mElapsedTSC);
             DrawText(strBuffer, widthSum, height, fontSize, BLACK);
-            widthSum += tableColumns[++colIndex].mWidth + widthPadding;
+            widthSum += tableColumns[++colIndex].mWidth + elementWidthPadding;
 
             std::sprintf(strBuffer, "%f", profilerReports[i].mElapsedTimeInS);
             DrawText(strBuffer, widthSum, height, fontSize, BLACK);
-            widthSum += tableColumns[++colIndex].mWidth + widthPadding;
+            widthSum += tableColumns[++colIndex].mWidth + elementWidthPadding;
 
             std::sprintf(strBuffer, "%f", profilerReports[i].mPercentage);
             DrawText(strBuffer, widthSum, height, fontSize, BLACK);
-            widthSum += tableColumns[++colIndex].mWidth + widthPadding;
+            widthSum += tableColumns[++colIndex].mWidth + elementWidthPadding;
 
             std::sprintf(strBuffer, "%f", profilerReports[i].mPercentageWithChildren);
             DrawText(strBuffer, widthSum, height, fontSize, BLACK);
-            widthSum += tableColumns[++colIndex].mWidth + widthPadding;
+            widthSum += tableColumns[++colIndex].mWidth + elementWidthPadding;
 
             std::sprintf(strBuffer, "%f", profilerReports[i].mProcessedMegabytes);
             DrawText(strBuffer, widthSum, height, fontSize, BLACK);
-            widthSum += tableColumns[++colIndex].mWidth + widthPadding;
+            widthSum += tableColumns[++colIndex].mWidth + elementWidthPadding;
 
             std::sprintf(strBuffer, "%f", profilerReports[i].mGigabytesPerSecond);
             DrawText(strBuffer, widthSum, height, fontSize, BLACK);
-            widthSum += tableColumns[++colIndex].mWidth + widthPadding;
+            widthSum += tableColumns[++colIndex].mWidth + elementWidthPadding;
         }
         EndDrawing();
     }
